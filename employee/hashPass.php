31a351e10a816ec372b19f3c0eb820cdf504f084
jbenedicto20.jbb@gmail.com <JBenedicto13>
2023-03-29 20:00:00 +0800

    Create hashPass.php

---
+++ employee/hashPass.php
@@ -0,0 +1,21 @@
+<?php 
+    //Set the hash
+    $PASSWORD = "mypassword";
+    $input = "mypassword";
+
+    $salt = "94665FAE66173BF677A723E4E38E5";
+    $hash_salt = hash_hmac("sha256", $PASSWORD, $salt);
+    $pwd_hashed = password_hash($hash_salt, PASSWORD_ARGON2ID); //save to database
+
+    //validating password
+    $salt = "94665FAE66173BF677A723E4E38E5";
+    $hash_salt = hash_hmac("sha256", $input, $salt);
+    if (password_verify($hash_salt, $pwd_hashed)) {
+    //CONFIRMED - CORRECT PASSWORD
+        echo "Correct";
+    } else {
+    //INCORRECT
+        echo "INCORRECT";
+    }
+
+?>

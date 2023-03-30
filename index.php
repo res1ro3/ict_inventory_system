0d8fdabb310420b6d69deecc9449d85a0bdba03b
res1ro3 <reserve1@ro3.dost.gov.ph>
2023-03-30 16:17:23 +0800

    Add nav tab in index.php

--- index.php
+++ index.php
@@ -11,28 +11,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 <body>
-    <h3>Management of Accounts</h3>
-    <table id="example" class="display table table-light" style="width:100%">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Username</th>
-                <th>Type of Account</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>jbenedicto13</td>
-                <td>Admin</td>
-                <td>
-                    <button class="btn btn-success">Edit</button>
-                    <button class="btn btn-warning">Reset Password</button>
-                    <button class="btn btn-danger">Archive</button>
-                </td>
-            </tr>
-    </table>
+    <div class="container">
+        <div class="navigation-tabs">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./inventory/manageInventory.php">Inventory</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./employee/manageAccounts.php">Employees</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </body>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>

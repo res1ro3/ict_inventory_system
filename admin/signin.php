abe055627da555e932c0938f829952ef3a9791a2
res1ro3 <reserve1@ro3.dost.gov.ph>
2023-04-20 11:22:27 +0800

    re create signin admin

--- admin/signin.php
+++ admin/signin.php
@@ -6,39 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign in</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/admin-signin.css">
+    <link rel="stylesheet" href="styles/admin-signin.css">
 </head>
 <body>
-    <div class="signin">
+    <div class="signin d-flex align-items-center flex-column">
         <div id="nav-placeholder"></div>
-        <div>
-            <form onsubmit="handleSubmit(e)" class="needs-validation signin-form" novalidate method="post">
-                <p class="fs-3 my-3">Sign In</p>
-                <div class="mb-3 form-floating">
-                    <input type="text" class="form-control" id="emailInp" name="emailInp" required autocomplete="TRUE">
-                    <label for="emailInp" class="form-label">Email Address</label>
-                    <div class="invalid-feedback">
-                        Please enter Email address
-                    </div>
-                </div>
-                <div class="mb-3 form-floating">
-                    <input type="password" class="form-control" id="passInp" name="passInp" required autocomplete="TRUE">
-                    <label for="passInp" class="form-label">Password</label>
-                    <div class="invalid-feedback">
-                        Please enter Password
-                    </div>
-                    <p class="btnshowpass" onclick="handleShowpass()">Show</p>
-                </div>
-                <button type="submit" class="btn btn-primary ">Signin</button>
-            </form>
-        </div>
+        <form onsubmit="handleSignin(); return false" class="signin-form mt-5 w-50 needs-validation" novalidate>
+            <h3 class="form-title">Sign in</h3>
+            <div class="mb-3">
+                <label class="form-label" for="usernameInp">Username</label>
+                <input class="form-control" type="text" name="usernameInp" id="usernameInp" placeholder="juan2345678" required>
+            </div>
+            <label class="form-label" for="passwordInp">Password</label>
+            <div class="input-group mb-3">
+                <input type="password" class="form-control" name="passwordInp" id="passwordInp" placeholder="********" aria-label="Example text with button addon" aria-describedby="button-addon1" required autocomplete>
+                <button onclick="showPass()" id='btn-show' class="btn btn-outline-secondary" type="button" id="button-addon1">Show</button>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary" type="submit">Signin</button>
+            </div>
+        </form>
+        <button onclick="handleSignin()">TEST</button>
     </div>
 </body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6952492a89.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-
         // Example starter JavaScript for disabling form submissions if there are invalid fields
         (() => {
         'use strict'
@@ -59,44 +55,32 @@
         })
         })();
 
-        function handleShowpass() {
-            passVal = document.getElementById('passInp').type;
-            if (passVal == 'text') {
-                document.getElementById('passInp').type = 'password';
+        $(document).ready(function() {
+            $("#nav-placeholder").load("../nav.html");
+        });
+
+        function showPass() {
+            btnType = document.getElementById("passwordInp").type;
+            if (btnType == "password") {
+                document.getElementById("passwordInp").type = "text";
             } else {
-                document.getElementById('passInp').type = 'text';
+                document.getElementById("passwordInp").type = "password";
             }
         }
 
-        const handleSubmit = (e) => {
-            e.preventDefault();
+        function handleSignin() {
             $.ajax({
-                type: "POST",
-                url: "./submit-signin.php",
-                data: {
-                    employee_id: $("#employeeId").val(),
-                },
-                success: function (res) {
-                    Swal.fire({
-                        title: 'Success!',
-                        text: res,
-                        icon: 'success',
-                        confirmButtonText: 'Okay'
-                    }).then(()=>location.reload())
-                },
-                error: function (res) {
-                    Swal.fire({
-                        title: 'Error!',
-                        text: res,
-                        icon: 'error',
-                        confirmButtonText: 'Okay'
-                    }).then(()=>location.reload())
-                }
-            });
+                    url: 'submit-signin.php',
+                    method: 'POST',
+                    data: {
+                        username: document.getElementById("usernameInp").value,
+                        password: document.getElementById("passwordInp").value,
+                    }
+
+                }).then((res) => {
+                    console.log(res);
+                });
         }
 
-        $(document).ready(function() {
-            $("#nav-placeholder").load("../nav.html");
-        })
     </script>
 </html>
--- admin/submit-signin.php
+++ admin/submit-signin.php
@@ -2,10 +2,24 @@
 
     require_once('../dbConfig.php');
 
-    if(isset($_POST['email']) && $_POST['pass']) {
-        echo $_POST['email'] . ' : ' . $_POST['pass'];
+    if(isset($_POST['username']) && $_POST['password']) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM employee_tbl WHERE username = :un AND password = :ps";
+        $query = $conn->prepare($sql);
+        $query->bindParam(':un',$username,PDO::PARAM_STR);
+        $query->bindParam(':ps',$password,PDO::PARAM_STR);
+        $query->execute();
+
+        if ($query->rowCount()) {
+            echo 
+        } else {
+            echo 'An error occurred';
+        }
+
     } else {
-        echo 'Failed';
+        echo 'Please fill all the fields';
     }
 
 ?>

6acebf848f1eb2961ad11a531f8eb218573f2a0e
res1ro3 <reserve1@ro3.dost.gov.ph>
2023-04-27 17:04:22 +0800

    add dasboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     if (isset($_SESSION) && $_SESSION['status'] == 'valid') {
-        // echo $_SESSION['username'];
+        $user = $_SESSION['username'];
     } else {
         header("Location: signin.php");
     }
@@ -12,60 +12,80 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="./styles/dashboard.css">
+    <title><?= $user ?></title>
+    <link rel="stylesheet" type="text/css" href="styles/dashboard.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 <body>
     <div class="dashboard">
-        <div class="sidebar d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px; height: 100vh;">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-            <span class="fs-4">Dashboard</span>
-            </a>
-            <hr>
-            <ul class="nav nav-pills flex-column mb-auto">
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                    <i class="fa-solid fa-users"></i>
-                    Dashboard
-                    </a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" aria-current="page">
-                    <i class="fa-solid fa-gauge"></i>
-                    Inventory
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                    <i class="fa-solid fa-boxes-stacked"></i>
-                    Inventory
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                    <i class="fa-solid fa-list-check"></i>
-                    Services
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                    <i class="fa-solid fa-user"></i>
-                    Profile
-                    </a>
-                </li>
-            </ul>
-            <hr>
-            <div class="text-center">
-                <a onclick="signout()" class="text-dark text-decoration-none" href="#">Signout</a>
+        <div id="sidebar-placeholder"></div>
+        <div class="container">
+        <h3>As of Today</h3>
+            <div class="data-group">
+                <div class="card">
+                    <div>
+                        <h1>12</h1>
+                    </div>
+                    <div>
+                        <p>No. of ICT Equipment to be replaced</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <h1>12</h1>
+                    </div>
+                    <div>
+                        <p>No. of ICT Equipment per unit vs number of employees</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <h1>10</h1>
+                    </div>
+                    <div>
+                        <p>No. of pending/ongoing technical support</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <h1>12</h1>
+                    </div>
+                    <div>
+                        <p>Information Systems</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <h1>12</h1>
+                    </div>
+                    <div>
+                        <p>Remaining available communal ICT resources to be borrowed</p>
+                    </div>
+                </div>
+            </div>
+<!-- Brand vs No. of repair -->
+            <h3>Brand vs Repair</h3>
+            <div class="data-group">
+                <div class="card">
+                    <div>
+                        <h1>12</h1>
+                    </div>
+                    <div>
+                        <p>Dell</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div>
+                        <h1>12</h1>
+                    </div>
+                    <div>
+                        <p>HP</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -75,6 +95,9 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
     <script>
+        $(document).ready(function() {
+            $('#sidebar-placeholder').load('../sidebar.html');
+        })
         const signout = () => {
             $.ajax({
                     url: 'signout.php',
--- employee/manageAccounts.php
+++ employee/manageAccounts.php
@@ -19,161 +19,165 @@
     <script src="../js/jquery.dataTables.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="styles/employee.css">
 </head>
 <body>
     <div class="container">
-        <div id="nav-placeholder"></div>
-        <h3>Management of Accounts</h3>
-        <button class="btn btn-dark mb-3" onclick="location.href='./addEmployee.php'">Add Account</button>
-        <table id="accsTbl" class="display table table-light" style="width:100%">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Username</th>
-                    <th>Type of Account</th>
-                    <th>Status</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $sql="SELECT * FROM employee_tbl";
-                    $query = $conn->prepare($sql);
-                    $query->execute();
-                    $result = $query->fetchAll();
-                    $count = 1;
-                    foreach ($result as $row) {
-                ?>
-                <tr>
-                    <td><?= $count ?></td>
-                    <span id="eid" style="display: none"><?= $row['employee_id']?></span>
-                    <td><?= $row['username'] ?></td>
-                    <td><?= $row['type_of_account'] ?></td>
-                    <td><?= $row['status'] ?></td>
-                    <td>
-                        <button id="editBtn" onclick="get('<?= $row['employee_id'] ?>')" type="button" data-id="<?= $row['employee_id'] ?>" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
-                        <button class="btn btn-warning">Reset Password</button>
-                        <button onclick="change_status('<?= $row['status'] ?>','<?= $row['employee_id'] ?>')" id="btnStatus" class="btn btn-danger"><?= $row['status'] == "Active" ? "Inactivate" : "Activate" ?></button>
-                    </td>
-                </tr>
-                <?php $count++; } ?>
-        </table>
+    <div style="position: absolute; left: 0; top: 0;" id="sidebar-placeholder"></div>
+        <div class="manageaccounts">
+            <!-- <div id="nav-placeholder"></div> -->
+            <h3>Management of Accounts</h3>
+            <button class="btn btn-dark mb-3" onclick="location.href='./addEmployee.php'">Add Account</button>
+            <table id="accsTbl" class="display table table-light" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Username</th>
+                        <th>Type of Account</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $sql="SELECT * FROM employee_tbl";
+                        $query = $conn->prepare($sql);
+                        $query->execute();
+                        $result = $query->fetchAll();
+                        $count = 1;
+                        foreach ($result as $row) {
+                    ?>
+                    <tr>
+                        <td><?= $count ?></td>
+                        <span id="eid" style="display: none"><?= $row['employee_id']?></span>
+                        <td><?= $row['username'] ?></td>
+                        <td><?= $row['type_of_account'] ?></td>
+                        <td><?= $row['status'] ?></td>
+                        <td>
+                            <button id="editBtn" onclick="get('<?= $row['employee_id'] ?>')" type="button" data-id="<?= $row['employee_id'] ?>" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
+                            <button class="btn btn-warning">Reset Password</button>
+                            <button onclick="change_status('<?= $row['status'] ?>','<?= $row['employee_id'] ?>')" id="btnStatus" class="btn btn-danger"><?= $row['status'] == "Active" ? "Inactivate" : "Activate" ?></button>
+                        </td>
+                    </tr>
+                    <?php $count++; } ?>
+            </table>
 
-        <!-- Edit Modal -->
-            <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="editModalLabel">Edit Account</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                <form class="needs-validation" novalidate id="updateAccForm" name="updateAccForm" method="post">
-                    <input type="hidden" id="employeeId" name="employeeId"></input>
-                    <div class="mb-3 row">
-                        <div class="mb-3 col form-floating">
-                            <input type="text" class="form-control addAccInp" id="lnameInp" name="lnameInp" required>
-                            <label for="lnameInp" class="form-label ps-4" id="lnameLbl">Last Name</label>
-                            <div class="invalid-feedback">
-                                Please enter Last Name
+            <!-- Edit Modal -->
+                <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="editModalLabel">Edit Account</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <form class="needs-validation" novalidate id="updateAccForm" name="updateAccForm" method="post">
+                        <input type="hidden" id="employeeId" name="employeeId"></input>
+                        <div class="mb-3 row">
+                            <div class="mb-3 col form-floating">
+                                <input type="text" class="form-control addAccInp" id="lnameInp" name="lnameInp" required>
+                                <label for="lnameInp" class="form-label ps-4" id="lnameLbl">Last Name</label>
+                                <div class="invalid-feedback">
+                                    Please enter Last Name
+                                </div>
+                            </div>
+                            <div class="mb-3 col form-floating">
+                                <input type="text" class="form-control addAccInp" id="fnameInp" name="fnameInp" required>
+                                <label for="fnameInp" class="form-label ps-4" id="fnameLbl">First Name</label>
+                                <div class="invalid-feedback">
+                                    Please enter First Name
+                                </div>
                             </div>
                         </div>
-                        <div class="mb-3 col form-floating">
-                            <input type="text" class="form-control addAccInp" id="fnameInp" name="fnameInp" required>
-                            <label for="fnameInp" class="form-label ps-4" id="fnameLbl">First Name</label>
-                            <div class="invalid-feedback">
-                                Please enter First Name
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="mb-3 form-floating">
-                        <input type="text" class="form-control addAccInp" id="usernameInp" name="usernameInp" required>
-                        <label for="usernameInp" class="form-label" id="usernameLbl">Username</label>
-                        <div class="invalid-feedback">
-                            Please enter Username
-                        </div>
-                        <span id="usernameInp-message"></span>
-                    </div>
                         
-                    <div class="mb-3 form-floating">
-                        <select class="form-select" id="sexInp" name="sexInp" required>
-                            <option value="" selected disabled>Select Sex</option>
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                        <label for="sexInp" id="sexLbl">Sex</label>
-                        <div class="invalid-feedback">
-                            Please select sex
+                        <div class="mb-3 form-floating">
+                            <input type="text" class="form-control addAccInp" id="usernameInp" name="usernameInp" required>
+                            <label for="usernameInp" class="form-label" id="usernameLbl">Username</label>
+                            <div class="invalid-feedback">
+                                Please enter Username
+                            </div>
+                            <span id="usernameInp-message"></span>
                         </div>
-                    </div>
-
-                    <div class="mb-3 form-floating">
-                        <select class="form-select" id="officeInp" name="officeInp" required>
-                            <option value="" selected disabled>Select Office</option>
-                        <?php
-                            $sql="SELECT * FROM office_tbl";
-                            $query = $conn->prepare($sql);
-                            $query->execute();
-                            $results=$query->fetchAll(PDO::FETCH_OBJ);
                             
-                            $count=1;
-                            if($query->rowCount() > 0) {
-                            //In case that the query returned at least one record, we can echo the records within a foreach loop:
-                                foreach($results as $result)
-                            {
-                        ?>
-                            <option value="<?php echo htmlentities($result->name);?>"><?php echo htmlentities($result->name);?></option>
-                        <?php }} ?>
-                        </select>
-                        <label for="officeInp" id="officeLbl">Office</label>
-                        <div class="invalid-feedback">
-                            Please select an Office
+                        <div class="mb-3 form-floating">
+                            <select class="form-select" id="sexInp" name="sexInp" required>
+                                <option value="" selected disabled>Select Sex</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                            <label for="sexInp" id="sexLbl">Sex</label>
+                            <div class="invalid-feedback">
+                                Please select sex
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="mb-3 form-floating">
-                        <select class="form-select" id="positionInp" name="positionInp" required>
-                            <option value="" selected disabled>Select Position</option>
-                            <option>Position 1</option>
-                            <option>Position 2</option>
-                            <option>Position 3</option>
-                        </select>
-                        <label for="positionInp" id="positionLbl">Position</label>
-                        <div class="invalid-feedback">
-                            Please select a Position
+                        <div class="mb-3 form-floating">
+                            <select class="form-select" id="officeInp" name="officeInp" required>
+                                <option value="" selected disabled>Select Office</option>
+                            <?php
+                                $sql="SELECT * FROM office_tbl";
+                                $query = $conn->prepare($sql);
+                                $query->execute();
+                                $results=$query->fetchAll(PDO::FETCH_OBJ);
+                                
+                                $count=1;
+                                if($query->rowCount() > 0) {
+                                //In case that the query returned at least one record, we can echo the records within a foreach loop:
+                                    foreach($results as $result)
+                                {
+                            ?>
+                                <option value="<?php echo htmlentities($result->name);?>"><?php echo htmlentities($result->name);?></option>
+                            <?php }} ?>
+                            </select>
+                            <label for="officeInp" id="officeLbl">Office</label>
+                            <div class="invalid-feedback">
+                                Please select an Office
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="mb-3 form-floating">
-                        <select class="form-select" id="typeOfEmploymentInp" name="typeOfEmploymentInp" required>
-                            <option value="" selected disabled>Select Type of Employment</option>
-                            <option>COS</option>
-                            <option>Regular</option>
-                        </select>
-                        <label for="typeOfEmploymentInp" id="typeOfEmploymentLbl">Type of Employment</label>
-                        <div class="invalid-feedback">
-                            Please select Type of Employment
+                        <div class="mb-3 form-floating">
+                            <select class="form-select" id="positionInp" name="positionInp" required>
+                                <option value="" selected disabled>Select Position</option>
+                                <option>Position 1</option>
+                                <option>Position 2</option>
+                                <option>Position 3</option>
+                            </select>
+                            <label for="positionInp" id="positionLbl">Position</label>
+                            <div class="invalid-feedback">
+                                Please select a Position
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="mb-3 form-floating">
-                        <select class="form-select" id="typeOfAccountInp" name="typeOfAccountInp" required>
-                            <option value="" selected disabled>Select Type of Account</option>
-                            <option>Admin</option>
-                            <option>Ordinary User</option>
-                        </select>
-                        <label for="typeOfAccountInp" id="typeOfAccounttLbl">Type of Account</label>
-                        <div class="invalid-feedback">
-                            Please select Type of Account
+                        <div class="mb-3 form-floating">
+                            <select class="form-select" id="typeOfEmploymentInp" name="typeOfEmploymentInp" required>
+                                <option value="" selected disabled>Select Type of Employment</option>
+                                <option>COS</option>
+                                <option>Regular</option>
+                            </select>
+                            <label for="typeOfEmploymentInp" id="typeOfEmploymentLbl">Type of Employment</label>
+                            <div class="invalid-feedback">
+                                Please select Type of Employment
+                            </div>
                         </div>
+
+                        <div class="mb-3 form-floating">
+                            <select class="form-select" id="typeOfAccountInp" name="typeOfAccountInp" required>
+                                <option value="" selected disabled>Select Type of Account</option>
+                                <option>Admin</option>
+                                <option>Ordinary User</option>
+                            </select>
+                            <label for="typeOfAccountInp" id="typeOfAccounttLbl">Type of Account</label>
+                            <div class="invalid-feedback">
+                                Please select Type of Account
+                            </div>
+                        </div>
+                    </form>
                     </div>
-                </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button onclick="update()" type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button onclick="update()" type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -183,6 +187,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/6952492a89.js" crossorigin="anonymous"></script>
     <script>
+        
         function get(eid) {
             $.ajax({
                 type: "GET",
@@ -269,7 +274,8 @@
 
         $(document).ready(function () {
             $('#accsTbl').DataTable();
-            $("#nav-placeholder").load("../nav.html");
+            // $("#nav-placeholder").load("../nav.html");
+            $('#sidebar-placeholder').load('../sidebar.html');
         });
     </script>
 </html>

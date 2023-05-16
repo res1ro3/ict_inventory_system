6c27eb28eb82805b8c344744432764cbd4c04e16
res1ro3 <reserve1@ro3.dost.gov.ph>
2023-05-16 16:28:10 +0800

    Add View modal with Ownership and Repair History

--- inventory/get.php
+++ inventory/get.php
@@ -3,6 +3,9 @@
 
     $mac_address = $_GET['mac_address'];
 
+    session_start();
+    $_SESSION['selected_mac'] = $mac_address;
+
     $sql = "
         SELECT mac_address, type_of_hardware, brand, model, serial_number, date_of_purchase, warranty, employee_tbl.employee_id, ict_network_hardware_tbl.status, ict_network_hardware_tbl.owner_name
         FROM ict_network_hardware_tbl
--- inventory/manageInventory.php
+++ inventory/manageInventory.php
@@ -71,6 +71,7 @@
                         <td><?= $row['owner_name'] //$row['lname'].', '.$row['fname'] ?></td>
                         <td><?= $row['status'] ?></td>
                         <td>
+                            <button id="viewBtn" onclick="get('<?= $row['mac_address'] ?>')" type="button" data-id="<?= $row['mac_address'] ?>" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#viewModal">View</button>
                             <button id="editBtn" onclick="get('<?= $row['mac_address'] ?>')" type="button" data-id="<?= $row['mac_address'] ?>" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
                             <button id="editBtn" onclick="getTransfer('<?= $row['mac_address'] ?>')" type="button" data-id="<?= $row['mac_address'] ?>" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#transferModal">Transfer</button>
                         </td>
@@ -126,10 +127,175 @@
             </table>
         </div>
     </div>
-        
+    
+    <!-- View Modal -->
+    <div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-dialog-scrollable">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="viewModalLabel">Edit ICT Network Hardware</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <form>
+                    <div class="col">
+                        <label for="macInp" class="form-label" id="macLbl">MAC Address</label>
+                        <input type="text" class="form-control" id="macInp" name="macInp" disabled>
+                    </div>
+                    <div class="mb-3 col">
+                        <label for="typeofhardwareInp" id="typeofhardwareLbl">Type of Hardware</label>
+                        <select class="form-select" id="typeofhardwareInp" name="typeofhardwareInp" disabled>
+                            <option value="" selected disabled>Select Type of Hardware</option>
+                            <option>Equipment</option>
+                            <option>Tools</option>
+                        </select>
+                    </div>
+                    <div class="mb-3 col">
+                        <label for="brandInp" id="brandLbl">Brand</label>
+                        <select class="form-select" id="brandInp" name="brandInp" disabled>
+                            <option value="" selected disabled>Please select Brand</option>
+                            <?php
+                            $sql="SELECT * FROM `brand_tbl`";
+                            $query = $conn->prepare($sql);
+                            $query->execute();
+                            $results=$query->fetchAll(PDO::FETCH_OBJ);
+                            
+                            $count=1;
+                            if($query->rowCount() > 0) {
+                            //In case that the query returned at least one record, we can echo the records within a foreach loop:
+                                foreach($results as $result)
+                            {
+                        ?>
+                            <option value="<?php echo htmlentities($result->name);?>"><?php echo htmlentities($result->name);?></option>
+                        <?php }} ?>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label for="modelInp" class="form-label" id="modelLbl">Model</label>
+                            <input type="text" class="form-control" id="modelInp" name="modelInp" disabled>
+                        </div>
 
+                        <div class="col">
+                            <label for="serialnumberInp" class="form-label" id="serialnumberLbl">Serial Number</label>
+                            <input type="text" class="form-control" id="serialnumberInp" name="serialnumberInp" disabled>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label for="dateofpurchaseInp" class="form-label ps-4" id="dateofpurchaseLbl">Date of Purchase</label>
+                            <input type="date" class="form-control" id="dateofpurchaseInp" name="dateofpurchaseInp" disabled>
+                        </div>
+
+                        <div class="col">
+                            <label for="warrantyInp" class="form-label ps-4" id="warrantyLbl">End of Warranty</label>
+                            <input type="date" class="form-control" id="warrantyInp" name="warrantyInp" disabled>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="ownerInpName" id="ownerNameLbl">Owner</label>
+                        <input type="text" class="form-control" id="ownerInpName" name="ownerInpName" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="statusInp" id="statusLbl">Status</label>
+                        <select class="form-select" id="statusInp" name="statusInp" disabled>
+                            <option value="" selected disabled>Please select Status</option>
+                            <option>Serviceable</option>
+                            <option>Non-Serviceable</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#ownershipHistoryDiv" aria-expanded="false" aria-controls="ownershipHistoryDiv">
+                                    Ownership History
+                                </button>
+                                </h2>
+                                <div id="ownershipHistoryDiv" class="accordion-collapse collapse">
+                                <div class="accordion-body">
+                                    <table class="table table-striped table-hover">
+                                        <thead>
+                                            <th>Date Transferred</th>
+                                            <th>Old Owner</th>
+                                            <th>New Owner</th>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                $mac_address =  $_SESSION['selected_mac'];
+                                                $sql="SELECT * FROM ict_transfer_tbl WHERE mac_address = :mac";
+                                                $query = $conn->prepare($sql);
+                                                $query->bindParam(':mac',$mac_address,PDO::PARAM_STR);
+                                                $query->execute();
+                                                $results=$query->fetchAll(PDO::FETCH_OBJ);
+                                                $count=1;
+                                                if($query->rowCount() > 0) {
+                                                //In case that the query returned at least one record, we can echo the records within a foreach loop:
+                                                    foreach($results as $result)
+                                                {
+                                            ?>
+                                            <tr>
+                                                <td><?= $result->date_transferred?></td>
+                                                <td><?= $result->old_owner?></td>
+                                                <td><?= $result->new_owner?></td>
+                                            </tr>
+                                        </tbody>
+                                        <?php }} ?>
+                                    </table>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#repairHistoryDiv" aria-expanded="false" aria-controls="repairHistoryDiv">
+                                    Repair History
+                                </button>
+                                </h2>
+                                <div id="repairHistoryDiv" class="accordion-collapse collapse">
+                                <div class="accordion-body">
+                                <table class="table table-striped table-hover">
+                                        <thead>
+                                            <th>Date Returned</th>
+                                            <th>Type of Service</th>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                $mac_address =  $_SESSION['selected_mac'];
+                                                $sql="SELECT * FROM services_tbl WHERE ICT_ID = :mac";
+                                                $query = $conn->prepare($sql);
+                                                $query->bindParam(':mac',$mac_address,PDO::PARAM_STR);
+                                                $query->execute();
+                                                $results=$query->fetchAll(PDO::FETCH_OBJ);
+                                                $count=1;
+                                                if($query->rowCount() > 0) {
+                                                //In case that the query returned at least one record, we can echo the records within a foreach loop:
+                                                    foreach($results as $result)
+                                                {
+                                            ?>
+                                            <tr>
+                                                <td><?= $result->date_returned?></td>
+                                                <td><?= $result->type_of_services?></td>
+                                            </tr>
+                                        </tbody>
+                                        <?php }} ?>
+                                    </table>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Encode Service</button>
+                    <button type="button" class="btn btn-primary">Transfer Ownership</button>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- Edit Modal -->
-            <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
@@ -330,12 +496,16 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/6952492a89.js" crossorigin="anonymous"></script>
     <script>
+        $('#viewModal').on('hidden.bs.modal', function () {
+        // refresh current page
+            location.reload();
+        })
         function get(mac) {
             $.ajax({
                 type: "GET",
                 url: "./get.php",
-                data: {mac_address:mac},
-                success: function (res) {
+                data: {mac_address:mac}
+            }).then((res) => {
                     res = JSON.parse(res);
                     $("#macInp").val(res.mac_address);
                     $("#typeofhardwareInp").val(res.type_of_hardware);
@@ -345,9 +515,9 @@
                     $('#dateofpurchaseInp').val(res.date_of_purchase);
                     $('#warrantyInp').val(res.warranty);
                     $('#ownerInp').val(res.employee_id);
+                    $('#ownerInpName').val(res.owner_name);
                     $('#statusInp').val(res.status);
-                }
-            });
+                });
         }
 
         function getTransfer(mac) {

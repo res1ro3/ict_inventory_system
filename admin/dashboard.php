420e7ab6b8bf7363b58d21229fb3a141ace1eb70
res1ro3 <reserve1@ro3.dost.gov.ph>
2023-05-17 14:25:00 +0800

    Populate the brands vs repairs chart

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,9 +1,25 @@
 <?php 
+    require_once('../dbConfig.php');
     session_start();
     if (isset($_SESSION) && $_SESSION['status'] == 'valid') {
         $user = $_SESSION['username'];
     } else {
         header("Location: signin.php");
+    }
+
+    function getRepairs($type, $brand) {
+        if ($type == 'Hardware') {
+            require('../dbConfig.php');
+            $sql = "SELECT * FROM ict_network_hardware_tbl WHERE brand = :br";
+            $query = $conn->prepare($sql);
+            $query->execute(array(
+                'br' => $brand
+            ));
+    
+            $res = $query->rowCount();
+    
+            echo $res;
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -31,13 +47,25 @@
                         </div>
                     </div>
                     <div class="col data-card">
-                        <div class="col-4 data-digit"><p>1</p></div>
+                        <?php 
+                            $sql = "SELECT * FROM services_tbl WHERE service_status = 'Pending'";
+                            $query = $conn->query($sql);
+                            $query -> execute();
+                            $pendingCount=$query->rowCount();
+                        ?>
+                        <div class="col-4 data-digit"><p><?= $pendingCount ?></p></div>
                         <div class="col-8 data-text">
                             <p>Pending technical support</p>
                         </div>
                     </div>
                     <div class="col data-card">
-                        <div class="col-4 data-digit"><p>2</p></div>
+                        <?php 
+                            $sql = "SELECT * FROM services_tbl WHERE service_status = 'On Going'";
+                            $query = $conn->query($sql);
+                            $query -> execute();
+                            $ongoingCount=$query->rowCount();
+                        ?>
+                        <div class="col-4 data-digit"><p><?= $ongoingCount ?></p></div>
                         <div class="col-8 data-text">
                             <p>Ongoing technical support</p>
                         </div>
@@ -62,49 +90,51 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        
+
         $(document).ready(function() {
-            var brands = [];
+            var brands_repair = [];
             $.ajax({
                     url: 'getBrands.php',
                     method: 'GET',
                 }).then((res) => {
-                    brands = JSON.parse(res);
+                    brands_repair = JSON.parse(res);
                     var ctx = document.getElementById("brands_chart").getContext('2d');
                     var myChart = new Chart(ctx, {
                         type: 'bar',
                         data: {
-                        labels: brands,
-                        datasets: [{
-                            label: 'Brand VS Repair',
-                            data: [12, 19, 3, 5, 2, 3, 6, 2, 3, 1, 4, 12, 1],
-                            backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                            ],
-                            borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                            ],
-                            borderWidth: 1
-                        }]
+                            labels: brands_repair[0],
+                            datasets: [{
+                                label: 'Brand VS Repair',
+                                data: brands_repair[1],
+                                backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
                         },
                         options: {
-                        scales: {
-                            yAxes: [{
-                            ticks: {
-                                beginAtZero: true
+                            scales: {
+                                y: {
+                                    min: 0,
+                                    max: 10,
+                                    ticks: {
+                                        stepsize: 1
+                                    }
+                                }
                             }
-                            }]
-                        }
                         }
                     });
                 });
--- admin/getBrands.php
+++ admin/getBrands.php
@@ -2,15 +2,38 @@
 
     require("../dbConfig.php");
 
+    function getRepairs($type, $brand) {
+        if ($type == 'Hardware') {
+            require('../dbConfig.php');
+            $sql = "SELECT * FROM ict_network_hardware_tbl WHERE brand = :br";
+            $query = $conn->prepare($sql);
+            $query->execute(array(
+                'br' => $brand
+            ));
+    
+            $res = $query->rowCount();
+            return $res;
+        }
+    }
+
     $sql = "SELECT * FROM brand_tbl";
     $query = $conn->prepare($sql);
     $query->execute();
+    $brandCount = $query->rowCount();
     
     $brands = [];
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         $brands[] = $row['name'];
     }
-    $json = json_encode($brands);
+
+    $repairs = [];
+
+    for ($i = 0; $i < $brandCount; $i++) {
+        $row = getRepairs("Hardware", $brands[$i]);
+        $repairs[] = $row;
+    }
+    
+    $json = json_encode([$brands, $repairs]);
     echo $json;
 
 ?>

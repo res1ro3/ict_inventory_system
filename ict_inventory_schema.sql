21006415a0d4c901849b24e0216d2f3f49b8772a
res1ro3 <reserve1@ro3.dost.gov.ph>
2023-06-02 17:01:20 +0800

    update db

--- ict_inventory_schema.sql
+++ ict_inventory_schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2023 at 09:41 AM
+-- Generation Time: Jun 02, 2023 at 11:00 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,7 +42,7 @@ CREATE TABLE `accessories_tbl` (
 --
 
 INSERT INTO `accessories_tbl` (`accessories_id`, `generic_name`, `employee_id`, `unit`, `quantity`, `specifications`, `brand`) VALUES
-(2, 'HDMI', 3, 'Office of the Regional Director', 3, 'HDMI v1', 'No Brand'),
+(2, 'HDMI', 8, 'Office of the Regional Director', 3, 'HDMI v1', 'No Brand'),
 (3, 'Headset', 2, 'Administrative Division', 4, 'HQ Sound', 'Logitech');
 
 -- --------------------------------------------------------
@@ -73,7 +73,8 @@ INSERT INTO `brand_tbl` (`brand_id`, `name`) VALUES
 (15, 'Intel'),
 (16, 'Microsoft'),
 (17, 'Linux'),
-(19, 'Brand X');
+(19, 'Brand X'),
+(26, 'Brand Y');
 
 -- --------------------------------------------------------
 
@@ -135,9 +136,9 @@ CREATE TABLE `ict_network_hardware_tbl` (
 --
 
 INSERT INTO `ict_network_hardware_tbl` (`hardware_id`, `mac_address`, `type_of_hardware`, `brand`, `model`, `serial_number`, `date_of_purchase`, `warranty`, `employee_id`, `status`, `owner_name`, `specifications`, `cost`) VALUES
-(1, '78-9F-BD-88-5E-A1', 'Desktop', 'Acer', 'Model A', 'sdfe4w23dsf', '2023-05-25', '2024-05-25', 5, 'Serviceable', '3', 'Specs here', 45000),
-(2, '68-9F-BD-88-5E-A0', 'Laptop', 'Dell', 'Model B', 'sdfe4w23dsg', '2023-05-25', '2024-05-25', 2, 'Non-Serviceable', '2', '8GB RAM, Ryzen 7', 100000),
-(3, '43-F8-6A-1T-2S-3A', 'Printer', 'HP', 'Printer X', 'p4r56i7n0t3e1r', '2023-05-25', '2024-05-25', 2, 'Serviceable', '8', 'High Quality Laser Printer, With Washing Machine', 30000);
+(1, '78-9F-BD-88-5E-A1', 'Desktop', 'Acer', 'Model A', 'sdfe4w23dsf', '2023-05-25', '2024-07-25', 3, 'Non-Serviceable', '3', 'Specs here', 45000),
+(2, '68-9F-BD-88-5E-A0', 'Laptop', 'Dell', 'Model B', 'sdfe4w23dsg', '2023-05-25', '2024-05-25', 5, 'Non-Serviceable', '2', '8GB RAM, Ryzen 7', 100000),
+(3, '43-F8-6A-1T-2S-3A', 'Printer', 'HP', 'Printer X', 'p4r56i7n0t3e1r', '2023-05-25', '2024-05-25', 5, 'Serviceable', '8', 'High Quality Laser Printer, With Washing Machine', 30000);
 
 -- --------------------------------------------------------
 
@@ -161,10 +162,11 @@ CREATE TABLE `ict_transfer_tbl` (
 --
 
 INSERT INTO `ict_transfer_tbl` (`transfer_id`, `employee_id_new`, `employee_id_old`, `ict_id`, `ict_type`, `date_transferred`, `new_owner`, `old_owner`) VALUES
-(51, 2, 8, 3, '', '2023-06-01', '', ''),
-(52, 5, 2, 1, '', '2023-06-01', '', ''),
-(53, 4, 2, 3, '', '2023-06-01', '', ''),
-(54, 3, 2, 2, '', '2023-06-01', '', '');
+(61, 5, 8, 1, 'hardware', '2023-06-02', '', ''),
+(62, 5, 3, 2, 'software', '2023-06-02', '', ''),
+(63, 2, 4, 2, 'accessories', '2023-06-02', '', ''),
+(64, 1, 5, 1, 'hardware', '2023-06-02', '', ''),
+(65, 3, 1, 1, 'hardware', '2023-06-02', '', '');
 
 -- --------------------------------------------------------
 
@@ -233,15 +235,17 @@ CREATE TABLE `services_tbl` (
 --
 
 INSERT INTO `services_tbl` (`services_id`, `type_of_services`, `ICT_ID`, `date_received`, `date_returned`, `description_of_service`, `action_done`, `remarks`, `recommendation`, `type_of_ict`, `employee_id`, `processed_by`, `service_status`) VALUES
-(27, 'Repair', '1', '2023-05-25', '', '123123123', 'Erase the RAM with pencil eraser', 'asdasdasd', 'Clean the unit', 'Hardware', 1, 'MIS Unit', 'Pending'),
-(29, 'Repair', '1', '2023-05-26', '2023-05-27', 'qwerty', 'dasdasd', 'asdasdasd', 'asdasdasd', 'Hardware', 1, 'MIS Unit', 'Finished'),
+(27, 'Maintenance', '1', '2023-05-25', '', '123123123', 'Erase the RAM with pencil eraser', 'asdasdasd', 'Clean the unit', 'Hardware', 1, 'MIS Unit', 'Finished'),
+(29, 'Repair', '1', '2023-06-02', '2023-05-27', 'qwerty', 'dasdasd', 'asdasdasd', 'asdasdasd', 'Hardware', 1, 'MIS Unit', 'Pending'),
 (30, 'Repair', '2', '2023-05-29', '', 'aas', 'dasdassdasd', 'asda', 'asdasd', 'Hardware', 1, 'MIS Unit', 'Pending'),
 (33, 'Repair', '1', '2023-05-29', '', 'asd', 'asdasdasd', 'qweqweqwe', '12341231', 'Hardware', 1, 'MIS Unit', 'Pending'),
 (34, 'Installation', '2', '2023-05-31', '2023-05-31', 'Install Photoshop', 'Install Photoshop', 'Install Photoshop', 'Install Photoshop', 'Software', 1, 'MIS Unit', 'Finished'),
 (36, 'Maintenance', '3', '2023-05-31', '', 'asd', 'asd', 'q2312', '123123', 'Hardware', 1, 'MIS Unit', 'Pending'),
 (37, 'Repair', '2', '2023-05-31', '', 'Can\'t Open', 'Can\'t Open', 'Can\'t Open', 'Can\'t Open', 'Software', 1, 'MIS Unit', 'Pending'),
 (41, 'Repair', '3', '2023-06-01', '', 'asdasd', 'asdasd', '123412', 'asdasd', 'Accessories', 1, 'MIS Unit', 'On Going'),
-(42, 'Repair', '2', '2023-06-08', '2023-06-04', 'asd123123123', 'asdasdasd', 'asdasdasdasd', 'as123123123', 'Accessories', 1, '123123123123', 'Finished');
+(42, 'Repair', '2', '2023-06-08', '2023-06-04', 'asd123123123', 'asdasdasd', 'asdasdasdasd', 'as123123123', 'Accessories', 1, '123123123123', 'Finished'),
+(47, 'Maintenance', '1', '2023-06-03', '', 'asd', 'asd', 'asd', 'asd', 'Tool/Supply', 1, 'asdasd', 'Pending'),
+(48, 'Maintenance', '2', '2023-06-29', '', 'asdasd', 'asdasd', 'asdas', 'dasd', 'Accessories', 1, 'MIS Unit', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -266,7 +270,7 @@ CREATE TABLE `software_tbl` (
 --
 
 INSERT INTO `software_tbl` (`software_id`, `software_name`, `date_developed_purchased`, `employee_id`, `type_of_software`, `type_of_subscription`, `manufacturer`, `type_developed`, `owner_name`) VALUES
-(2, 'Photoshop 2023', '2023-10-10', 3, 'Editing', 'Yearly', 'Adobe', 'Lorem Ipsum', 'JB'),
+(2, 'Photoshop 2023', '2023-10-10', 5, 'Editing', 'Yearly', 'Adobe', 'Lorem Ipsum', 'JB'),
 (3, 'Illustrator', '2023-05-31', 4, 'Editing', 'Monthly', 'Adobe', '', ''),
 (4, 'asd', '2023-05-31', 5, 'Productivity', 'Monthly', 'asdasd', '', '');
 
@@ -277,13 +281,23 @@ INSERT INTO `software_tbl` (`software_id`, `software_name`, `date_developed_purc
 --
 
 CREATE TABLE `supplies_tools_tbl` (
-  `supply_tools_id` varchar(64) NOT NULL,
+  `supply_tools_id` int(8) NOT NULL,
   `type_of_supply_tools` varchar(64) NOT NULL,
   `employee_id` int(8) NOT NULL,
   `unit` varchar(32) NOT NULL,
   `quantity` int(11) NOT NULL,
   `specifications_remarks` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supplies_tools_tbl`
+--
+
+INSERT INTO `supplies_tools_tbl` (`supply_tools_id`, `type_of_supply_tools`, `employee_id`, `unit`, `quantity`, `specifications_remarks`) VALUES
+(1, 'type 1', 4, 'Office of the Regional Director', 2, 'sadasdasd'),
+(2, 'asdasd', 2, 'Administrative Division', 2, '789'),
+(3, 'SSSS', 3, 'Administrative Division', 9, 'sDSDSDasdas'),
+(4, 'asd', 5, 'Office of the Regional Director', 2, 'asd');
 
 -- --------------------------------------------------------
 
@@ -301,17 +315,58 @@ CREATE TABLE `type_of_hardware_tbl` (
 --
 
 INSERT INTO `type_of_hardware_tbl` (`type_of_hardware_id`, `name`) VALUES
-(1, 'Desktop'),
+(1, 'desktop b'),
 (2, 'Laptop'),
 (3, 'Printer'),
 (4, 'Scanner'),
 (5, 'NAS'),
-(6, 'CCTV'),
+(6, 'NAS'),
 (7, 'Server'),
 (8, 'Router'),
 (9, 'Switch'),
 (10, 'Firewall'),
-(11, 'Conference Microphone System');
+(11, 'Conference Microphone System'),
+(31, ' desktop');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_of_software_tbl`
+--
+
+CREATE TABLE `type_of_software_tbl` (
+  `type_of_software_id` int(8) NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `type_of_software_tbl`
+--
+
+INSERT INTO `type_of_software_tbl` (`type_of_software_id`, `name`) VALUES
+(1, 'Editing'),
+(2, 'Productivity'),
+(3, 'Information System');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_of_subscription_tbl`
+--
+
+CREATE TABLE `type_of_subscription_tbl` (
+  `type_of_subscription_id` int(8) NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `type_of_subscription_tbl`
+--
+
+INSERT INTO `type_of_subscription_tbl` (`type_of_subscription_id`, `name`) VALUES
+(1, 'Yearly'),
+(2, 'Monthly'),
+(10, 'Quarterly');
 
 --
 -- Indexes for dumped tables
@@ -382,6 +437,7 @@ ALTER TABLE `software_tbl`
 -- Indexes for table `supplies_tools_tbl`
 --
 ALTER TABLE `supplies_tools_tbl`
+  ADD PRIMARY KEY (`supply_tools_id`),
   ADD KEY `fk_employee_id_supplies` (`employee_id`);
 
 --
@@ -389,6 +445,18 @@ ALTER TABLE `supplies_tools_tbl`
 --
 ALTER TABLE `type_of_hardware_tbl`
   ADD PRIMARY KEY (`type_of_hardware_id`);
+
+--
+-- Indexes for table `type_of_software_tbl`
+--
+ALTER TABLE `type_of_software_tbl`
+  ADD PRIMARY KEY (`type_of_software_id`);
+
+--
+-- Indexes for table `type_of_subscription_tbl`
+--
+ALTER TABLE `type_of_subscription_tbl`
+  ADD PRIMARY KEY (`type_of_subscription_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -404,7 +472,7 @@ ALTER TABLE `accessories_tbl`
 -- AUTO_INCREMENT for table `brand_tbl`
 --
 ALTER TABLE `brand_tbl`
-  MODIFY `brand_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `brand_id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `employee_tbl`
@@ -422,7 +490,7 @@ ALTER TABLE `ict_network_hardware_tbl`
 -- AUTO_INCREMENT for table `ict_transfer_tbl`
 --
 ALTER TABLE `ict_transfer_tbl`
-  MODIFY `transfer_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `transfer_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `office_tbl`
@@ -434,7 +502,7 @@ ALTER TABLE `office_tbl`
 -- AUTO_INCREMENT for table `services_tbl`
 --
 ALTER TABLE `services_tbl`
-  MODIFY `services_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `services_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `software_tbl`
@@ -443,10 +511,28 @@ ALTER TABLE `software_tbl`
   MODIFY `software_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `supplies_tools_tbl`
+--
+ALTER TABLE `supplies_tools_tbl`
+  MODIFY `supply_tools_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `type_of_hardware_tbl`
 --
 ALTER TABLE `type_of_hardware_tbl`
-  MODIFY `type_of_hardware_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `type_of_hardware_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `type_of_software_tbl`
+--
+ALTER TABLE `type_of_software_tbl`
+  MODIFY `type_of_software_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `type_of_subscription_tbl`
+--
+ALTER TABLE `type_of_subscription_tbl`
+  MODIFY `type_of_subscription_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
